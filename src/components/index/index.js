d914244c64cc2881e4120ec57fd3a2a18142d483
wangxiaoqing <xiaoqingwang@126.com>
2019-01-10 22:46:01 +0800

    图片不对

--- src/components/index/index.js
+++ src/components/index/index.js
@@ -14,17 +14,19 @@ const initFiles = [
     'name': 'IMG_20180803_181224.jpg',
     'size': 4266973,
     'uuid': '53e6d67e-ea73-41d2-8780-9798a474e45f',
-    'thumbnailUrl': 'http://yt.i2u.top:4101/fineuploads/53e6d67e-ea73-41d2-8780-9798a474e45f/IMG_20180803_181224.jpg',
+    'thumbnailUrl': 'http://29e5534ea20a8.cdn.sohucs.com/c_zoom,h_213/c_cut,x_32,y_0,w_600,h_400/os/news/99f9b0799cac0675cd1df356629e0568.jpg',
   },
   {
     'id':3,
     'name': 'IMG_20180803_181348.jpg',
     'size': 2799364,
     'uuid': '3432d592-ceaf-45ff-b8a4-42802bed0723',
-    'thumbnailUrl': 'IMG_20180803_181224.jpg'
+    'thumbnailUrl': 'http://29e5534ea20a8.cdn.sohucs.com/c_zoom,h_213/c_cut,x_32,y_0,w_600,h_400/os/news/99f9b0799cac0675cd1df356629e0568.jpg'
   },
 ]
 
+//接口应该用如下的：
+// const initFiles = [ 'http://29e5534ea20a8.cdn.sohucs.com/c_zoom,h_213/c_cut,x_32,y_0,w_600,h_400/os/news/99f9b0799cac0675cd1df356629e0568.jpg','http://29e5534ea20a8.cdn.sohucs.com/c_zoom,h_213/c_cut,x_32,y_0,w_600,h_400/os/news/99f9b0799cac0675cd1df356629e0568.jpg'];
 const uploader = new FineUploaderTraditional({
   options: {
       chunking: {
@@ -46,10 +48,10 @@ const uploader = new FineUploaderTraditional({
 class Index extends Component {
   constructor(props) {
     super(props);
-    
+
   }
   render() {
-    
+
     return (
       <div style={{
         backgroundPositionX: 'center',
